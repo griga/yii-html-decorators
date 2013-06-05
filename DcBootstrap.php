2030768b90e5053b4db51857c6be34d59a17ff35
Yura Griga <grigach@gmail.com>
2013-06-05 13:31:15 +0300

    initial

---
+++ DcBootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/** Created by griga at 30.05.13 | 21:27.
+ * 
+ */
+
+class DcBootstrap {
+	public 
+}
---
+++ bootstrap/forms/RowWidget.php
@@ -0,0 +1,22 @@
+<?php
+/** Created by griga at 30.05.13 | 21:29.
+ * 
+ */
+
+class RowWidget extends CWidget {
+	public $label = '';
+	public $for = '';
+	public function init()
+	{
+		echo CHtml::openTag('div', array('class' => 'control-group'));
+		echo CHtml::label($this->label, $this->for, array('class' => 'control-label'));
+		echo CHtml::openTag('div', array('class' => 'controls'));
+	}
+
+	public function run(){
+		echo CHtml::closeTag('div');
+		echo CHtml::closeTag('div');
+	}
+
+
+}
